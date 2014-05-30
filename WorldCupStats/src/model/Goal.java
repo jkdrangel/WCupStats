2e5fed382a1fb8b61b098c6518a5de109152502f
Oto <yo.tamashii@gmail.com>
2014-05-30 02:12:34 -0300

    Criação da classe Goal(gol)

---
+++ WorldCupStats/src/model/Goal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ * Classe que representa os gols da selecao de um pais em uma copa.
+ * 
+ * @author D.L.O.L
+ * 
+ * @version 1.0
+ * 
+ * @see 
+ * @see 
+ */
+public class Goal {
+    
+    /**
+     * Construtor da classe.
+     * 
+     * @param 
+     */
+    public Goal() {
+        
+    }
+}
