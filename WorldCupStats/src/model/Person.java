422d6a096b6309b26507790a0869c564b33c9c24
Oto <yo.tamashii@gmail.com>
2014-05-30 02:05:25 -0300

    Criação da classe abstrata Person(Pessoa)

---
+++ WorldCupStats/src/model/Person.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ * Interface que representa uma pessoa (Tecnico ou Jogador).
+ * 
+ * @author D.L.O.L
+ * 
+ * @see 
+ * @see 
+ */
+public abstract class Person {
+    
+    /**
+     * Nome da pessoa. 
+    */ private String name;
+    /**
+     * Data de aniversario da pessoa.
+     */ private String dateBirth;
+
+     
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the dateBirth
+     */
+    public String getDateBirth() {
+        return dateBirth;
+    }
+
+    /**
+     * @param dateBirth the dateBirth to set
+     */
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+    
+}
