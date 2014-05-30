0c61ddff94461c716ba6a57cf463a742309146d5
Oto <yo.tamashii@gmail.com>
2014-05-30 03:18:03 -0300

    Inicio do desenvolvimento de Team

--- WorldCupStats/src/model/Team.java
+++ WorldCupStats/src/model/Team.java
@@ -6,6 +6,9 @@
 
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Classe que representa a selecao de um pais em um determinado
  * ano de copa.
@@ -19,12 +22,101 @@ package model;
  */
 public class Team {
     
+    /** 
+     * Grupo da selecao em uma determinada copa.
+    */ private final char group; 
+    /**
+     * Ano em que o time esteve na copa (ID);
+     */ private final int ano; 
+    /**
+     * Classificacao do time em uma determinada copa.
+     */ private final String rank;
+     /**
+      * Tecnico da selecao em um ano de copa.
+     */private final Person coach;
+     /**
+      * Selecao de jogadores para uma copa.
+     */private List<Person> players;
+     /**
+      * Gols feitos pela selecao em uma copa.
+     */private List<Goal> goals;
+          
+     
+     
     /**
      * Construtor da classe.
      * 
-     * @param 
+     * @param group
+     * @param ano
+     * @param rank 
+     * @param coach 
      */
-    public Team() {
+    public Team(char group, int ano, String rank, Person coach) {
         
+        this.group = group;
+        this.ano = ano;
+        this.rank = rank;
+        this.coach = coach;
+        
+        players = new ArrayList<Person>();
+        goals = new ArrayList<Goal>();
     }
+
+    /**
+     * @return the group
+     */
+    public char getGroup() {
+        return group;
+    }
+
+    /**
+     * @return the ano
+     */
+    public int getAno() {
+        return ano;
+    }
+
+    /**
+     * @return the rank
+     */
+    public String getRank() {
+        return rank;
+    }
+
+    /**
+     * @return the coach
+     */
+    public Person getCoach() {
+        return coach;
+    }
+
+    /**
+     * @return the players
+     */
+    public List<Person> getPlayers() {
+        return players;
+    }
+
+    /**
+     * @param player the players to insert
+     */
+    public void addPlayers(Person player) {
+        players.add(player);
+    }
+
+    /**
+     * @return the goals
+     */
+    public List<Goal> getGoals() {
+        return goals;
+    }
+
+    /**
+     * @param goal the goals to insert
+     */
+    public void addGoals(Goal goal) {
+        goals.add(goal);
+    }
+    
+    
 }
