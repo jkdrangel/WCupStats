6041d865b8b6c2dc681d78f618574e7ebd7888c0
Oto <yo.tamashii@gmail.com>
2014-05-30 01:28:29 -0300

    Criação das Classes Game(Jogo), Substituition(Substituição) e Team(Seleção)

---
+++ WorldCupStats/src/model/Game.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ * Classe que representa um jogo entre duas selecoes em uma copa.
+ * 
+ * @author D.L.O.L
+ * 
+ * @version 1.0
+ * 
+ * @see 
+ * @see 
+ */
+public class Game {
+ 
+    
+    /**
+     * Construtor da classe.
+     * 
+     * @param 
+     */
+    public Game() {
+        
+    }
+}
---
+++ WorldCupStats/src/model/Substitution.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ * Classe que representa uma substituicao, de uma determinada
+ * selecao, realizada em um jogo.
+ * 
+ * @author D.L.O.L
+ * 
+ * @see 
+ * @see 
+ */
+public class Substitution {
+    
+    /**
+     * Construtor da classe.
+     * 
+     * @param 
+     */
+    public Substitution() {
+        
+    }
+}
