1d0c4d951f2fa5bc2f299dbfce4a76a9d7018be7
Diego Leite <diegojleite@gmail.com>
2014-05-31 15:38:24 -0300

    Criacao da classe Escalacao, com atributos, getters e setters.

--- WorldCupStats/src/model/Escalacao.java
+++ WorldCupStats/src/model/Escalacao.java
@@ -8,16 +8,78 @@
 
 package model;
 
+import java.util.List;
+
 /**
- * Classe que representa os gols da selecao de um pais em uma copa.
+ * Classe que representa a Escalacao de uma Selecao durante um Jogo.
  * 
  * @author D.L.O.L
  * 
  * @version 1.0
  * 
- * @see 
- * @see 
+ * @see Jogo
+ * @see Jogadores
  */
 public class Escalacao {
+    private int ID;
+    private Jogo jogo;
+    private List<Jogador> jogadores;
     
+    /**
+     * Construtor da classe.
+     * @param jogo, Jogo em que os jogadores foram Escalados.
+     * @param jogadores, Jogadores que foram Escalados para o Jogo.
+     */
+    public Escalacao(Jogo jogo, List<Jogador> jogadores){
+        this.jogo = jogo;
+        this.jogadores = jogadores;
+    }
+    
+    /**
+     * Getter para o ID da classe.
+     * @return, Retorna o ID da classe.
+     */
+    public int getID(){
+        return this.ID;
+    }
+    
+    /**
+     * Setter para o ID da classe.
+     * @param ID, ID da classe.
+     */
+    public void setID(int ID){
+        this.ID = ID;
+    }
+    
+    /**
+     * Getter para o Jogo da classe.
+     * @return, Retorna o jogo em que o Gol foi feito.
+     */
+    public Jogo getJogo(){
+        return this.jogo;
+    }
+    
+    /**
+     * Setter para o Jogo da classe.
+     * @param jogo, Jogo em que o Gol foi feito.
+     */
+    public void setJogo(Jogo jogo){
+        this.jogo = jogo;
+    }
+    
+    /**
+     * Getter para os Jogadores da classe.
+     * @return, Retorna o Jogador que fez o Gol.
+     */
+    public List<Jogador> getJogador(){
+        return this.jogadores;
+    }
+    
+    /**
+     * Setter para os Jogadores da classe.
+     * @param jogador, Jogador que fez o Gol.
+     */
+    public void setJogador(List<Jogador> jogadores){
+        this.jogadores = jogadores;
+    }
 }
