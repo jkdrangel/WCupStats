144fd66e0cae720f142d723960f220a8c84c78e2
Oto <yo.tamashii@gmail.com>
2014-05-31 02:10:23 -0300

    Criação dos enuns

---
+++ WorldCupStats/src/model/FaseCopa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ * Fases de uma copa.
+ *
+ * @author D.L.O.L
+ *
+ * @version 1.0
+ *
+ * @see
+ * @see
+ */
+public enum FaseCopa {
+    
+    OITAVAS("Oitavas de Final"),
+    QUARTAS("Quartas de Final"), 
+    SEMI("Semifinal"), 
+    FINAL("Final");
+    
+
+    /**
+     * Fase na copa.
+     */
+    private final String fase;
+            
+    /**
+     * Construtor do Enum.
+     * 
+     * @param funcao 
+     */
+    private FaseCopa(String fase) {
+        this.fase = fase;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getFase() {
+        return fase;
+    }
+    }
