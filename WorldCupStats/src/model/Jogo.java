86c7a8cd3a18a893b441d91a9dd5fc2fcfb5b3ab
Oto <yo.tamashii@gmail.com>
2014-05-31 02:25:58 -0300

    Desenvolvimento da classe jogo

--- WorldCupStats/src/model/Jogo.java
+++ WorldCupStats/src/model/Jogo.java
@@ -3,28 +3,64 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
+
+import java.util.List;
+import model.Enuns.FaseCopa;
 
 /**
  * Classe que representa um jogo entre duas selecoes em uma copa.
- * 
+ *
  * @author D.L.O.L
- * 
+ *
  * @version 1.0
- * 
- * @see 
- * @see 
+ *
+ * @see
+ * @see
  */
 public class Jogo {
- 
-    
+
     /**
-     * Construtor da classe.
-     * 
-     * @param 
+     * fase do jogo na copa.
      */
+    private final FaseCopa FASE;
+    /**
+     * Data do jogo.
+     */
+    private final String data;
+    /**
+     * Lugar onde ocorreu o jogo.
+     */
+    private final String local;
+    /**
+     * Copa que houve este jogo.
+     */
+    private final Copa copa;
+    /**
+     * Time A.
+     */
+    private final Time timeA;
+    /**
+     * Time B.
+     */
+    private final Time timeB;
+    /**
+     * Escalacao do time A.
+     */
+    private final Escalacao escalacaoA;
+    /**
+     * Escalacao do time B.
+     */
+    private final Escalacao escalacaoB;
+    /**
+     * Lista de substituicoes no jogo.
+     */
+    private List<Substituicao> substis;
+
+    
+     // Construtor da classe.
+     
     public Jogo() {
-        
+
     }
 }
