940918c73863cbafc6247f1bc10f31859138ee52
lucassantana <lucassantana@slave-03>
2014-06-18 13:10:41 -0300

    Mudanças em copaDAO

--- WorldCupStatsWeb/WorldCupStats/test/model/CRUD/CopaDAOTest.java
+++ WorldCupStatsWeb/WorldCupStats/test/model/CRUD/CopaDAOTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 import model.pojo.Copa;
 import model.pojo.Pais;
 import model.pojo.Selecao;
-import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +21,7 @@ import org.junit.Test;
 public class CopaDAOTest {
 
     CopaDAO copaDAO;
+    PaisDAO paisdao;
     Copa copa, copa1, copa2;
     Pais brasil, africa, coreia;
     Selecao selecao;
@@ -29,19 +29,26 @@ public class CopaDAOTest {
     @Before
     public void setUp() throws Exception {
         copaDAO = new CopaDAO();
+        paisdao = new PaisDAO();
+        brasil = new Pais();
+        africa = new Pais();
+        coreia = new Pais();
+        paisdao.adicionar(brasil);
+        paisdao.adicionar(africa);
+        paisdao.adicionar(coreia);
 
         copa = new Copa();
-        copa.setAno(new Date(2014,0,0));
+        copa.setAno(new Date(2014, 0, 0));
         copa.setPais(brasil);
         copa.setSelecao(selecao);
 
         copa1 = new Copa();
-        copa1.setAno(new Date(2010,0,0));
+        copa1.setAno(new Date(2010, 0, 0));
         copa1.setPais(africa);
         copa1.setSelecao(selecao);
 
         copa2 = new Copa();
-        copa2.setAno(new Date(2002,0,0));
+        copa2.setAno(new Date(2002, 0, 0));
         copa2.setPais(coreia);
         copa2.setSelecao(selecao);
 
@@ -59,7 +66,7 @@ public class CopaDAOTest {
     @Test
     public void testAtualizar() {
         copaDAO.adicionar(copa);
-        copa.setAno(new Date(2013,0,0));
+        copa.setAno(new Date(2013, 0, 0));
         copaDAO.atualizar(copa);
         List<Copa> copas = copaDAO.listar();
         assertEquals(copa, copas.get(0));
