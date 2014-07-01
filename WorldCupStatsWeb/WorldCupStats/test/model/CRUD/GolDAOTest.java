26707d3e26eb486f974d9077887652a34b8f00ae
Diego Leite <diegojleite@gmail.com>
2014-06-30 21:27:29 -0300

    Testes de adicionar a atualizar prontos.

--- WorldCupStatsWeb/WorldCupStats/test/model/CRUD/GolDAOTest.java
+++ WorldCupStatsWeb/WorldCupStats/test/model/CRUD/GolDAOTest.java
@@ -7,6 +7,7 @@
 package model.CRUD;
 
 import java.util.Date;
+import java.util.List;
 import model.pojo.Gol;
 import model.pojo.Jogador;
 import model.pojo.Jogo;
@@ -76,82 +77,22 @@ public class GolDAOTest {
         golDAO.removerTodos();
     }
 
-    /**
-     * Test of getID method, of class GolDAO.
-     */
     @Test
-    public void testGetID() {
-        System.out.println("getID");
-        GolDAO instance = null;
-        int expResult = 0;
-        int result = instance.getID();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getJogo method, of class GolDAO.
-     */
-    @Test
-    public void testGetJogo() {
-        System.out.println("getJogo");
-        GolDAO instance = null;
-        JogoDAO expResult = null;
-        JogoDAO result = instance.getJogo();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getJogador method, of class GolDAO.
-     */
-    @Test
-    public void testGetJogador() {
-        System.out.println("getJogador");
-        GolDAO instance = null;
-        PessoaDAO expResult = null;
-        PessoaDAO result = instance.getJogador();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getSelecao method, of class GolDAO.
-     */
-    @Test
-    public void testGetSelecao() {
-        System.out.println("getSelecao");
-        GolDAO instance = null;
-        SelecaoDAO expResult = null;
-        SelecaoDAO result = instance.getSelecao();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTempo method, of class GolDAO.
-     */
-    @Test
-    public void testGetTempo() {
-        System.out.println("getTempo");
-        GolDAO instance = null;
-        Date expResult = null;
-        Date result = instance.getTempo();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isFoiContra method, of class GolDAO.
-     */
-    @Test
-    public void testIsFoiContra() {
-        System.out.println("isFoiContra");
-        GolDAO instance = null;
-        boolean expResult = false;
-        boolean result = instance.isFoiContra();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    public void testAdicionar(){
+        golDAO.adicionar(gol1);
+        List<Gol> gols = golDAO.listar();
+        assertEquals(gol1, gols.get(0));
     }
     
+    @Test
+    public void testAtualizar(){
+        golDAO.adicionar(gol1);
+        golDAO.adicionar(gol2);
+        golDAO.adicionar(gol3);
+        
+        gol2.setJogador(jogador2);
+        golDAO.atualizar(gol2);
+        List<Gol> gols = golDAO.listar();
+        assertEquals(gol2, gols.get(1));
+    }
 }
