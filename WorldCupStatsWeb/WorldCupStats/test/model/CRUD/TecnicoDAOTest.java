af2af226a8ce67b9e9293843a63b276f90b74cca
lucasvinicius <lucasvinicius@lucasvinicius-HuronRiver-Platform>
2014-07-01 11:58:59 -0300

    Teste do TecnicoDao finalizado.

--- WorldCupStatsWeb/WorldCupStats/test/model/CRUD/TecnicoDAOTest.java
+++ WorldCupStatsWeb/WorldCupStats/test/model/CRUD/TecnicoDAOTest.java
@@ -6,10 +6,16 @@
 
 package model.CRUD;
 
+import java.sql.Date;
+import java.util.List;
+import model.Enuns.FuncaoJogador;
+import model.pojo.Jogador;
+import model.pojo.Selecao;
+import model.pojo.Tecnico;
 import org.junit.After;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -17,42 +23,96 @@ import static org.junit.Assert.*;
  */
 public class TecnicoDAOTest {
     
-    public TecnicoDAOTest() {
-    }
     
+    TecnicoDAO tecnicoDao;
+    Tecnico felipao, titi, zagalo;
+    Selecao selecao;
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        tecnicoDao = new TecnicoDAO();
+       
+        felipao = new Tecnico();
+        felipao.setNome("Felipao");
+        felipao.setDataNascimento(new Date(100,2,1));
+        
+        titi = new Tecnico();
+        titi.setNome("Titi");
+        titi.setDataNascimento(new Date(100,1,1));
+        
+        zagalo = new Tecnico();
+        zagalo.setNome("Zagalo");
+        zagalo.setDataNascimento(new Date(100,3,1));
+
+        tecnicoDao.removerTodos();
+       
     }
-    
-    @After
+     @After
     public void tearDown() {
+        tecnicoDao.removerTodos();
     }
 
     /**
-     * Test of equals method, of class TecnicoDAO.
+     * Teste simples de adicao de componentes em banco de dados
      */
     @Test
-    public void testEquals() {
-        System.out.println("equals");
-        Object o = null;
-        TecnicoDAO instance = null;
-        boolean expResult = false;
-        boolean result = instance.equals(o);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    public void testAdicionar() {
+        tecnicoDao.adicionar(felipao);
+        List<Tecnico> jogadores = tecnicoDao.listar();
+        assertEquals(felipao, jogadores.get(0));
     }
 
     /**
-     * Test of hashCode method, of class TecnicoDAO.
+     * Verifica se o nome do tecnico foi atualizado no banco de dados
      */
     @Test
-    public void testHashCode() {
-        System.out.println("hashCode");
-        TecnicoDAO instance = null;
-        int expResult = 0;
-        int result = instance.hashCode();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    public void testAtualizar() {
+        tecnicoDao.adicionar(felipao);
+        List<Tecnico> tecnicos = tecnicoDao.listar();
+        assertEquals("Felipao", tecnicos.get(0).getNome());
+        felipao.setNome("Tecnico Felipao");
+        
+        tecnicoDao.atualizar(felipao);
+        tecnicos = tecnicoDao.listar();
+        assertEquals(" Tecnico Felipao", tecnicos.get(0).getNome());
+    }
+
+    @Test
+    public void testRemover() {
+        tecnicoDao.adicionar(felipao);
+        tecnicoDao.remover(felipao);
+        List<Tecnico> tecnicos = tecnicoDao.listar();
+        assertTrue(tecnicos.isEmpty());
+    }
+
+    @Test
+    public void testRemoverTodos() {
+        tecnicoDao.removerTodos();
+        List<Tecnico> tecnicos = tecnicoDao.listar();
+        assertTrue(tecnicos.isEmpty());
+    }
+
+    @Test
+    public void listar() {
+        tecnicoDao.adicionar(felipao);
+        tecnicoDao.adicionar(titi);
+        tecnicoDao.adicionar(zagalo);
+
+        List<Tecnico> tecnicos = tecnicoDao.listar();
+        assertFalse(tecnicos.isEmpty());
+        assertEquals(felipao, tecnicos.get(0));
+        assertEquals(titi, tecnicos.get(1));
+        assertEquals(zagalo, tecnicos.get(2));
+    }
+
+    @Test
+    public void buscar() {
+        tecnicoDao.adicionar(felipao);
+        tecnicoDao.adicionar(titi);
+        tecnicoDao.adicionar(zagalo);
+
+        Tecnico resultadoBusca = tecnicoDao.buscar(titi.getNome());
+        assertEquals(titi, resultadoBusca);
     }
     
 }
